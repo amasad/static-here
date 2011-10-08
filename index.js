5ea3720887d1b17f2d980b9f4193ee4789ef1a38
Amjad Masad <amjad.masad@gmail.com>
2011-10-08 21:16:34 +0300

    Tested and fixed coffee watch option. Redirect directory requests. Add watchFile fix option.

--- index.js
+++ index.js
@@ -2,23 +2,28 @@ var fs = require('fs'),
     http = require('http'),
     path = require('path'),
     url = require('url'),
-    EventEmitter = require('events').EventEmitter
+    EventEmitter = require('events').EventEmitter,
+    exec = require('child_process').exec,
     spawn = require('child_process').spawn;
 
 var args = {},
     argv = process.argv.slice(2);
-    
+
+// Guess arguments.
 for (var i = 0; i < argv.length; i++){
   arg = argv[i];
-  if (arg.match(/^d+$/)){
+  if (arg.match(/^\d+$/)){
     args.port = arg;
   } else if (arg === 'coffee'){
     args.coffee = true;
+  } else if (arg === 'fix'){
+    args.fix = true;
   } else {
     args.host = arg;
   }
 }
-  
+
+// Emulate mime if it didn't exist.
 var mime;
 try {
   mime = require('mime');
@@ -47,28 +52,41 @@ try {
   })();
 }
 
-
-function textResponse(res, code, txt) {
+// Simple http response.
+function httpRespond(res, code, txt, headers) {
+  headers = headers || {};
   txt = txt || '';
-  res.writeHead(code, {"Content-Type": "text/plain"});
+  headers['Content-Type'] = "text/plain";
+  res.writeHead(code, headers);
   res.end(txt);
 }
 
 var httpCb = function (req, res) {
   var uri = url.parse(req.url).pathname,
       filename = path.join(process.cwd(), uri);
-  
+
   path.exists(filename, function (exists) {
     if (!exists) {
-      textResponse(res, 404, "Page Not Found!\n");
+      httpRespond(res, 404, "Page Not Found!\n");
       return;
     }
     
-    if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+    if (fs.statSync(filename).isDirectory()) {
+      if (filename.slice(-1) !== '/') {
+        // Directory with out a trailing slash.
+        // redirect http://host/directory to http://host/directory/
+        httpRespond(res, 302, 'Location is a folder, redirecting..', {
+          'Location': uri + '/'
+        });
+        return;
+      } else {
+        filename = path.join(filename, 'index.html');
+      }
+    }
     
     fs.readFile(filename, 'binary', function (err, file) {
       if (err) {
-        textResponse(res, 500, err + '\n');
+        httpRespond(res, 500, err + '\n');
         return;
       }
       var ext = path.extname(filename).slice(1);
@@ -79,6 +97,15 @@ var httpCb = function (req, res) {
   }); 
 };
 
+// Assign defaults and define the start server action.
+args.port = args.port || 8888;
+args.host = args.host || '0.0.0.0';
+var startServer = function () {
+  http.createServer(httpCb).listen(args.port, args.host);
+  console.log('Serving files from %s at http://%s:%s/', process.cwd(), args.host, args.port);
+};
+
+// Check the coffee flag to start watching files.
 var coffee;
 if (args.coffee) {
   try {
@@ -87,51 +114,48 @@ if (args.coffee) {
   if (coffee) {
     exec("find . -name '*.coffee'", function (err, files) {
      if (err) throw err;
-     startWatching(files.split(/\n/));
-    })
+     files = files.split(/\n/).filter(function (file) {
+       return !!(file.trim().length);
+     });
+     startWatching(files);
+    });
   }
 }
 
-var Log = new EventEmitter();
-var log = function () {
-  Log.emit('out');
-  console.log.apply(console, [].slice.call(arguments));
-};
-
+// Watch an array of coffee files.
 var startWatching = function (files) {
-  compileCoffee = function (filname) {
-    log("Compiling " + filename);
-    var coffee_src = fs.readFileSync(filename, 'utf8'),
-        js_src = coffee.compile(coffee_src);
-    fs.writeFileSync(filename.replace(/\.cofffee$/, '.js'), js_src);
+  if (!files.length) {
+    startServer();
+    return;
   }
-
-  for (var i = 0; i < files.length; i++) {
-    compileCoffee(files[i]);
-    fs.watchFile(filename, function (curr, old) {
-      if (+current.mtime != +old.mtime) compileCoffee(filename);
-    });
-  }
-};
-
-var server_started = false,
-    timer;
-    
-args.port = args.port || 8888;
-args.host = args.host || '0.0.0.0';
-
-var start_timer = function () {
-  timer = setTimeout(function () {
-    http.createServer(httpCb).listen(args.port, args.host);
-    console.log('Serving files from' + process.cwd() + 'at http://' + args.host + ':' + args.port + '/');
-    server_started = true;
-  },  3000);
-};
-start_timer();
   
-Log.on('out', function () {
-  if (server_started) return;
-  clearTimeout(timer);
-  start_timer();
-});
+  // Compile a single coffee file.
+  var compileCoffee = function (filename) {
+    console.log("Compiling %s", filename);
+    var coffee_src = fs.readFileSync(filename, 'utf8'),
+        js_src;
+    
+    try{
+      js_src = coffee.compile(coffee_src);
+    } catch(e) {
+      console.log("Error compiling %s : %s", filename, e.message);
+      return;
+    }
+    fs.writeFileSync(filename.replace(/\.coffee$/, '.js'), js_src);
+  };
+  
+  // initial compile pass and watch files.
+  files.forEach(function (filename) {
+    compileCoffee(filename);
+    console.log("Watching %s", filename);
+    
+    fs.watchFile(filename, {interval:args.fix ? -1 : 0}, function (curr, old) {
+      if (+curr.mtime != +old.mtime) compileCoffee(filename);
+    });
+    startServer();
+  });
+};
+
+// The coffee feature will take care of starting the server.
+if (!args.coffee) startServer();
 
