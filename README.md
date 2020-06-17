95eade15173f01c4ef96da7ddf38d63c65b090c0
Amjad Masad <amjad.masad@gmail.com>
2020-06-17 03:38:20 +0000

    clean up and modernize

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-##static-here
-___________
-Create a static web server from the current directory.  
+## static-here
+
+A dead-simple static server for the current directory
 
 ### Install
 
@@ -8,11 +8,11 @@ Create a static web server from the current directory.
 
 ### Run
 
-  static-here
-    > Serving files from /badass/javascript/project at http://localhost:8888/
+    static-here
+    > Serving files from /home/me/project at http://localhost:8888/
     
 ### Options:
 
-  You can specify the port and hostname but defaults to:
+  You can specify the port and hostname in any order, however it defaults to:
   
-    static-here coffee 8888 0.0.0.0
+    static-here 8080 0.0.0.0
--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-var fs = require('fs'),
-    http = require('http'),
-    path = require('path'),
-    url = require('url'),
-    exec = require('child_process').exec,
-    mime = require('mime');
+const fs = require('fs');
+const http = require('http');
+const path = require('path');
+const url = require('url');
+const mime = require('mime');
 
-var args = {},
-    argv = process.argv.slice(2);
+const args = {
+  host: '0.0.0.0',
+  port: 8080,
+};
+const argv = process.argv.slice(2);
+const cwd = process.cwd();
 
 // Guess arguments.
-for (var i = 0; i < argv.length; i++){
-  arg = argv[i];
-  if (arg.match(/^\d+$/)){
+for (const arg of argv) {
+  if (arg.match(/^\d+$/)) {
     args.port = arg;
   } else {
     args.host = arg;
   }
 }
 
-// Simple http response.
-function httpRespond(res, code, txt, headers) {
+function respond(res, code, txt, headers) {
   headers = headers || {};
   txt = txt || '';
   headers['Content-Type'] = "text/plain";
@@ -27,49 +28,46 @@ function httpRespond(res, code, txt, headers) {
   res.end(txt);
 }
 
-var httpCb = function (req, res) {
-  var uri = url.parse(req.url).pathname,
-      filename = path.join(process.cwd(), uri);
+http.createServer((req, res) => {
+  const { pathname } = url.parse(req.url);
+  let filename = path.join(cwd, pathname);
 
-  fs.exists(filename, function (exists) {
-    if (!exists) {
-      httpRespond(res, 404, "Page Not Found!\n");
+  fs.stat(filename, (err, stats) => {
+    if (err) {
+      respond(res, 404, "Page Not Found!\n");
       return;
     }
-    
-    if (fs.statSync(filename).isDirectory()) {
+
+    if (stats.isDirectory()) {
       if (filename.slice(-1) !== '/') {
         // Directory with out a trailing slash.
         // redirect http://host/directory to http://host/directory/
-        httpRespond(res, 302, 'Location is a folder, redirecting..', {
-          'Location': uri + '/'
+        respond(res, 302, 'Location is a folder, redirecting..', {
+          'Location': pathname + '/'
         });
         return;
       } else {
         filename = path.join(filename, 'index.html');
       }
     }
-    
-    fs.readFile(filename, 'binary', function (err, file) {
+
+    fs.readFile(filename, 'binary', function(err, file) {
       if (err) {
-        httpRespond(res, 500, err + '\n');
+        respond(res, 500, err + '\n');
         return;
       }
-      var ext = path.extname(filename).slice(1);
-      res.writeHead(200, {'Content-Type': mime.lookup(ext)});
+
+      res.writeHead(200, { 'Content-Type': mime.getType(filename) });
       res.write(file, 'binary');
       res.end();
     });
   });
-};
+}).listen(args.port, args.host, () => {
+  console.log(
+    'Serving files from %s at http://%s:%s/',
+    process.cwd(),
+    args.host,
+    args.port
+  );
+});
 
-// Assign defaults and define the start server action.
-args.port = args.port || 8888;
-args.host = args.host || '0.0.0.0';
-http.createServer(httpCb).listen(args.port, args.host);
-console.log(
-  'Serving files from %s at http://%s:%s/',
-  process.cwd(),
-  args.host,
-  args.port
-);
